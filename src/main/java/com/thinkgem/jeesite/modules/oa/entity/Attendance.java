0f5019cb52b44d0794e3a6a5a3a3cfac4c1c8f66
Mojun <mojun.zheng@hongshenol.com>
2017-10-12 14:53:08 +0800

    upload Attendance

---
+++ src/main/java/com/thinkgem/jeesite/modules/oa/entity/Attendance.java
@@ -0,0 +1,65 @@
+package com.thinkgem.jeesite.modules.oa.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * 考勤
+ */
+@Document(collection = "test")
+public class Attendance {
+
+	@Id
+	private String id;
+
+	private String name;
+
+	// 月份
+	private String month;
+
+	// 考勤状态
+	private String attStatus;
+
+	// 删除标记
+	private String del_flag;
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public String getAttStatus() {
+		return attStatus;
+	}
+
+	public void setAttStatus(String attStatus) {
+		this.attStatus = attStatus;
+	}
+
+	public String getDel_flag() {
+		return del_flag;
+	}
+
+	public void setDel_flag(String del_flag) {
+		this.del_flag = del_flag;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
